1917474c2e1af17b2b78ee05f9a2be4f91c7d2b1
rubensantoscodacy <ruben.santos@codacy.com>
2022-06-17 15:46:02 +0100

    add code

--- main.m
+++ main.m
@@ -4,6 +4,7 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        NSLog(@"Hello, World!");
     }
     return 0;
 }
